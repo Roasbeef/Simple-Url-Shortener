d5e4ed92dc02a392a3c30f5cd8b21d2fc5afe025
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-07-18 17:48:32 -0500

    look ma, memcache

--- views.py
+++ views.py
@@ -6,6 +6,7 @@ import jinja2
 import re
 
 from models import Link
+from google.appengine.api import memcache
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -33,8 +34,8 @@ class LinkShortener(BaseHandler):
             URL_RE = re.compile(r"(https|http)")
             if not URL_RE.match(url):
                 url = 'http://%s' % url
-            u = Link.all().filter("url =", url).get()
-            #if url is already in datastore
+            u = memcache.get(url)
+            #if url is already cached
             if u:
                 params = {'hash': str(u.hash), 'link': str(u.url)}
                 self.render("url_form.html", **params)
@@ -42,8 +43,9 @@ class LinkShortener(BaseHandler):
                 try:
                     l = Link()
                     l.seed(url)
-                    h = l.hash
-                    params = {'hash': h, 'link': str(l.url)}
+                    memcache.set(url, l)
+                    memcache.set(str(l.hash), l)
+                    params = {'hash': str(l.hash), 'link': str(l.url)}
                     self.render("url_form.html", **params)
                 except ValueError:
                     params = {'error': True}
@@ -56,10 +58,11 @@ class LinkShortener(BaseHandler):
 class LinkRedirect(BaseHandler):
 
     def get(self, hash):
-        l = Link.all().filter("hash =", hash).get()
+        l = memcache.get(hash)
         if l:
             l.hits += 1
             l.put()
+            memcache.set(str(l.hash), l)
             self.redirect('%s' % str(l.url))
         else:
             self.redirect('/short')
